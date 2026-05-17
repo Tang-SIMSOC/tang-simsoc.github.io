dc5bc2024ea52be8e3690699fccf91ef8234c270
Tang-SIMSOC <19401434+Tang-SIMSOC@users.noreply.github.com>
2026-05-17 18:51:28 +0000

    Deploying to gh-pages from @ Tang-SIMSOC/tang-simsoc.github.io@dd23c2c76538e98830c9c42aa9ed7959900a24dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "An overview of ongoing and past projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
@@ -27,9 +34,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-empirical-investigation-of-negative-influence",
+          title: 'Empirical Investigation of Negative Influence',
+          description: "Using Stochastic Actor-Oriented Models for an empirically grounded agent-based model",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "teachings-data-science-fundamentals",
